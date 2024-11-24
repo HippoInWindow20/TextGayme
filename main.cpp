6ce3d68f709e976bc0c38c39f69a03441c92d42f
HippoInWindow20 <shippo.hsu@gmail.com>
2024-11-24 15:54:08 +0800

    class

--- main.cpp
+++ main.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Character
 {
-    private:
-
+    protected:
+        int attack;
+        int critAttack;
+        int critChance;
+        int speed;
+        int defense;
+        int hp;
+        int exp;
+        string name;
     public:
         
 };
+
+class You : public Character {
+    private:
+        int level;
+    public:
+}
 
 int main (){
     cout << "SUck my duck"<<endl;
