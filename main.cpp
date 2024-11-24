9952c2cc9a24d000ddd297bfe42a74c0cfb95366
antony000555 <antony000555@gmail.com>
2024-11-24 23:32:50 +0800

    allow for creating character

--- main.cpp
+++ main.cpp
@@ -2,31 +2,112 @@
 #include <string>
 using namespace std;
 
+const int FLOOR_CNT = 100;
+
+class Item
+{
+    protected:
+		string name;
+		int attackChange;
+        int critAttackChange;
+        int critChanceChange;
+        int speedChange;
+        int defenseChange;
+        int hpChange;
+    public:
+        Item(string name, int attackChange, int critAttackChange, int critChanceChange, int speedChange, int defenseChange, int hpChange);
+};
+
+Item::Item(string name, int attackChange, int critAttackChange, int critChanceChange, int speedChange, int defenseChange, int hpChange) : 
+    name(name), attackChange(attackChange), critAttackChange(critAttackChange), critChanceChange(critChanceChange), speedChange(speedChange), 
+    defenseChange(defenseChange), hpChange(hpChange)
+{}
+
 class Character
 {
     protected:
+        string name;
         int attack;
         int critAttack;
         int critChance;
         int speed;
         int defense;
         int hp;
-        int exp;
-        string name;
     public:
+        void attackOpponent(Character* opponent);
         
+        int getHP();
+        string getName();
 };
 
-class You : public Character {
-    private:
-        int level;
-    public:
+int Character::getHP()
+{
+    return hp;
 }
 
-int main (){
-    cout << "SUck my duck"<<endl;
-    cout << "duck";
-    cout << "DUCK";
-    cout << "fuck";
+string Character::getName()
+{
+    return name;
+}
+
+class Player : public Character {
+    protected:
+        int level;
+        int exp;
+        Item* handsOn;
+
+    public:
+        Player(string name);
+        ~Player();
+        void levelUp();
+        void equipItem(string itemName);
+};
+
+Player::Player(string name)
+{
+    this->name = name;
+    this->attack = 5;
+    this->critAttack = 10;
+    this->critChance = 0.1;
+    this->speed = 1;
+    this->defense = 1;
+    this->hp = 0;
+    this->level = 1;
+    this->exp = 0;
+}
+
+Player::~Player()
+{
+    delete[] handsOn;
+}
+
+void Player::equipItem(string itemName)
+{
+
+}
+
+int main()
+{
+    
+    string name;
+    cout << "Please name your character: ";
+    cin >> name;
+    Player player(name);
+
+    // Item listing
+    Item note7("Note 7", 2, 2, 0.01, 0, 0, 0);
+    Item nuclearBomb("Nuclear Bomb", 1, 10, 0, 0, 0, 0);
+    Item knife("Knife", 1, 2, 0, 0, 0, 0);
+
+    // game
+    for (int i = 0; i < FLOOR_CNT; i++)
+    {
+        if (player.getHP() <= 0)
+        {
+            cout << player.getName() << " sucks.";
+            break;
+        }
+    }
+    
     return 0;
 }
